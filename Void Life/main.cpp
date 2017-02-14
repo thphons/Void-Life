c628bac1816b4ff14b0214211c65570cb593c53c
Cal McLelland <thphons@gmail.com>
2017-02-14 10:04:24 -0500

    Initial Commit

---
+++ Void Life/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Void Life
+//
+//  Created by Cal McLelland on 17-02-14.
+//  Copyright (c) 2017 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
